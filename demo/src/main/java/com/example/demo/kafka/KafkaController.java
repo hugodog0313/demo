cb7eebb5caa16f90962111eb2c642b1c010dad03
hugodog0313 <71684522+hugodog0313@users.noreply.github.com>
2021-03-22 14:38:33 +0800

    s

--- demo/src/main/java/com/example/demo/kafka/KafkaController.java
+++ demo/src/main/java/com/example/demo/kafka/KafkaController.java
@@ -8,16 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class KafkaController {
 
-  @Autowired KafkaSender kafkaSender;
 
   @Autowired
   Producer producer;
 
-//  @GetMapping("/producer")
   @PostMapping("/producer")
   public String producer(@RequestParam String message) {
     producer.sendMessages(message);
-//    kafkaSender.send(message);
     return "Message sent to the Kafka Topic java_in_use_topic Successfully";
   }
 }
